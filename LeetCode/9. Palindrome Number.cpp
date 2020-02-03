5b5c360fae53fb61849248d65646daafabdae831
LaChimere <LaChimere@github.com>
2020-02-03 17:28:32 +0800

    Update 9. Palindrome Number.cpp

--- LeetCode/9. Palindrome Number.cpp
+++ LeetCode/9. Palindrome Number.cpp
@@ -2,12 +2,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        bool flag = true;
         string s = to_string(x);
         string t = s;
         reverse(t.begin(), t.end());
-        if (s != t)
-            flag = false;
-        return flag;
+        return s == t;
     }
 };
