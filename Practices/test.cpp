855439e48d677a416214521b6be330885b5b5142
LaChimere <LaChimere@github.com>
2020-02-08 16:23:56 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
 int main() {
-    vector<int> num = {0, 5, 2, 1, 4, 3};
-    for (auto it = num.begin(); it != num.end(); it++)
-        printf("%d ", *it);
-    auto it = num.begin();
-    printf("\n%d", *it);
-    int x = *it++;
-    printf("\n%d %d", x, *it);
+    vector<int> num = {0, 3, 2, 3, 4, 3};
+    for (int i = 0; i < num.size(); i++)
+        cout << num[i] << " ";
     return 0;
 }
