3b640849c22eb76f34d058fde5e58ad1c70bf82a
LaChimere <LaChimere@github.com>
2020-02-08 22:10:29 +0800

    Update 28. Implement strStr().cpp

--- LeetCode/28. Implement strStr().cpp
+++ LeetCode/28. Implement strStr().cpp
@@ -3,8 +3,6 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         if (needle.empty()) return 0;
-        else if (haystack.find(needle) != string::npos)
-            return haystack.find(needle);
-        else return -1;
+        else return haystack.find(needle);
     }
 };
