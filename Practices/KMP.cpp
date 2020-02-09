8f7df639228f2c539ff816e780a2da057b86aaa5
LaChimere <LaChimere@github.com>
2020-02-09 15:24:28 +0800

    Update KMP.cpp

--- Practices/KMP.cpp
+++ Practices/KMP.cpp
@@ -14,18 +14,20 @@ void getNext(int *next, char *s, int len) {
         next[j] = j;
     }
 }
-bool KMP(char *text, char *pattern, int *next) {
+int KMP(char *text, char *pattern, int *next) {
     // KMP算法核心部分
     int len1 = strlen(text), len2 = strlen(pattern);
     getNext(next, pattern, len2);
-    int j = -1;
+    int j = -1, ans = 0;    // ans为成功匹配的次数
     for (int i = 0; i < len1; i++) {
         while (j != 1 && text[i] != pattern[j+1])
             j = next[j];
         if (text[i] == pattern[j+1])
             j++;
-        if (j == len2 - 1)
-            return true;
+        if (j == len2 - 1) {
+            ans++;
+            j = next[j];    // j回到next[j]处继续匹配
+        }
     }
-    return false;
+    return ans;
 }
