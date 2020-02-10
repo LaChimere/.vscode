fdb161ba718462e0dea9160ccb1589ac1e8619b9
LaChimere <LaChimere@github.com>
2020-02-10 16:50:01 +0800

    Create 100. Same Tree.cpp

---
+++ LeetCode/Easy/100. Same Tree.cpp
@@ -0,0 +1,34 @@
+#include "tools"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        queue<TreeNode*> qu_p, qu_q;
+        qu_p.push(p);
+        qu_q.push(q);
+        while (!qu_p.empty() && !qu_p.empty()) {
+            TreeNode *frontP = qu_p.front();
+            TreeNode *frontQ = qu_q.front();
+            qu_p.pop();
+            qu_q.pop();
+            if (frontP != NULL && frontQ != NULL && frontP->val == frontQ->val) {
+                qu_p.push(frontP->left);
+                qu_p.push(frontP->right);
+                qu_q.push(frontQ->left);
+                qu_q.push(frontQ->right);
+            } else if (frontP == NULL && frontQ == NULL) {
+                continue;
+            } else return false;
+        }
+        if (qu_p.empty() && qu_q.empty())
+            return true;
+        else return false;
+    }
+};
