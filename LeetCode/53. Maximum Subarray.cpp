f75d552a7f725186793779e31dadcab0d12c65c7
LaChimere <LaChimere@github.com>
2020-02-10 11:10:54 +0800

    Update 53. Maximum Subarray.cpp

--- LeetCode/53. Maximum Subarray.cpp
+++ LeetCode/53. Maximum Subarray.cpp
@@ -4,11 +4,9 @@ public:
     int maxSubArray(vector<int>& nums) {
         vector<int> dp(nums.size());
         dp[0] = nums[0];
+        int sum = dp[0];
         for (int i = 1; i < nums.size(); i++) {
             dp[i] = max(nums[i], dp[i-1] + nums[i]);
-        }
-        int sum = INT_MIN;
-        for (int i = 0; i < nums.size(); i++) {
             if (dp[i] > sum)
                 sum = dp[i];
         }
