9de023eaab4b62bf6b75538d93d2b3760c008e4c
LaChimere <LaChimere@github.com>
2020-02-10 12:29:46 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -4,6 +4,12 @@
 using namespace std;
 
 int main() {
-    cout << int(string::npos);
+    vector<int> v(3, 100);
+    for (int i = 0; i < v.size(); i++)
+        printf("%d ", v[i]);
+    printf("\n");
+    v.insert(v.begin(), 200);
+    for (int i = 0; i < v.size(); i++)
+        printf("%d ", v[i]);
     return 0;
 }
