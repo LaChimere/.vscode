cd92c9e75c5471e4ae86848719377906ce5e2a51
LaChimere <LaChimere@github.com>
2020-02-12 16:45:29 +0800

    Update 122. Best Time to Buy and Sell Stock II.cpp

--- LeetCode/Easy/122. Best Time to Buy and Sell Stock II.cpp
+++ LeetCode/Easy/122. Best Time to Buy and Sell Stock II.cpp
@@ -11,5 +11,17 @@ public:
             }
         }
         return max_profit;
+        
+        // Dynamic Programming
+        // int n = prices.size();
+        // if (n < 2) return 0;
+        // vector<vector<int>> dp(n);
+        // dp[0].push_back(0);
+        // dp[0].push_back(-prices[0]);
+        // for (int i = 1; i < n; i++) {
+        //     dp[i].push_back(max(dp[i - 1][0], dp[i - 1][1] + prices[i]));
+        //     dp[i].push_back(max(dp[i - 1][1], dp[i - 1][0] - prices[i]));
+        // }
+        // return dp[n - 1][0];
     }
 };
