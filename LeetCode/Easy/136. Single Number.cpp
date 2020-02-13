c6d2e5f3362f454890486a7ba2df76d0131be4d1
LaChimere <LaChimere@github.com>
2020-02-13 10:01:50 +0800

    Create 136. Single Number.cpp

---
+++ LeetCode/Easy/136. Single Number.cpp
@@ -0,0 +1,24 @@
+#include "tools"
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        // using Hash Table, time complexity: O(n), space complexity: O(n)
+        // unordered_map<int, int> hash;
+        // int result = nums[0];
+        // hash[nums[0]] = 1;
+        // for (int i = 1; i < nums.size(); i++) {
+        //     if (hash.find(nums[i]) != hash.end())
+        //         hash[nums[i]]++;
+        //     else hash[nums[i]] = 1;
+        // }
+        // for (auto it = hash.begin(); it != hash.end(); it++)
+        //     if (it->second == 1) result = it->first;
+        // return result;
+        
+        // exclusive OR
+        int result = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+            result = result ^ nums[i];
+        return result;
+    }
+};
