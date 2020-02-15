8d449a8d104bd9f6940db4bfe19e94df29b54510
LaChimere <LaChimere@github.com>
2020-02-15 11:13:37 +0800

    Create 189. Rotate Array.cpp

---
+++ LeetCode/Easy/189. Rotate Array.cpp
@@ -0,0 +1,15 @@
+#include "tools"
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (n < 2) return;
+        k = k % n;
+        reverse(nums.begin(), nums.begin() + n - k);
+        reverse(nums.begin() + n - k, nums.end());
+        reverse(nums.begin(), nums.end());
+        
+        // rotate函数
+        // std::rotate(nums.rbegin(), nums.rbegin() + k, nums.rend());
+    }
+};
