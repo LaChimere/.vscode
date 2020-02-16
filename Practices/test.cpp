6813aa0ca778b4efeaf1e50c1f04dee7cda88c39
LaChimere <LaChimere@github.com>
2020-02-16 16:35:01 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -6,10 +6,19 @@
 #include <string>
 #include <unordered_map>
 #include <map>
+#include <set>
+#include <unordered_set>
 using namespace std;
 
 int main() {
-    if (nullptr == NULL)
-        cout << "true";
+    unordered_set<int> myset;
+    cout << myset.size() << endl;
+    myset.insert(1);
+    myset.insert(3);
+    myset.insert(4);
+    for (auto it = myset.begin(); it != myset.end(); it++)
+        cout << *it << " ";
+    cout << endl;
+    cout << myset.count(2);
     return 0;
 }
