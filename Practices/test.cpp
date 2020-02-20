93baa6c530b6e5d0e15a9ac68a875f3c2f7ad25e
LaChimere <LaChimere@github.com>
2020-02-20 14:09:04 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -11,14 +11,12 @@
 using namespace std;
 
 int main() {
-    unordered_set<int> myset;
-    cout << myset.size() << endl;
-    myset.insert(1);
+    unordered_multiset<int> myset;
     myset.insert(3);
-    myset.insert(4);
+    myset.insert(5);
+    myset.insert(3);
     for (auto it = myset.begin(); it != myset.end(); it++)
         cout << *it << " ";
-    cout << endl;
     cout << myset.count(2);
     return 0;
 }
