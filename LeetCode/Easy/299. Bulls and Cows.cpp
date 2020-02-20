c881dd2fe64085ec061ca682c0f7ab3e8ddb8a38
LaChimere <LaChimere@github.com>
2020-02-20 14:09:00 +0800

    Create 299. Bulls and Cows.cpp

---
+++ LeetCode/Easy/299. Bulls and Cows.cpp
@@ -0,0 +1,41 @@
+#include "tools"
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bull = 0, cow = 0;
+        int bucket[10] = {0};
+        size_t len = secret.length();
+        for (int i = 0; i < len; i++) {
+            if (guess[i] == secret[i]) {
+                bull++;
+                continue;
+            }
+            bucket[secret[i] - '0']++;
+            bucket[guess[i] - '0']--;
+        }
+        for (int num : bucket)
+            if (num > 0)
+                cow += num;
+        cow = len - cow - bull;
+        return to_string(bull) + "A" + to_string(cow) + "B";
+        
+        // int bull = 0, cow = 0;
+        // int secretNum[10] = {0}, checked[10] = {0};
+        // size_t len = secret.length();
+        // for (int i = 0; i < len; i++)
+        //     secretNum[secret[i] - '0']++;
+        // for (int i = 0; i < len; i++) {
+        //     if (guess[i] == secret[i]) {
+        //         bull++;
+        //         checked[guess[i] - '0']++;
+        //     }
+        // }
+        // for (int i = 0; i < len; i++) {
+        //     if (guess[i] != secret[i] && checked[guess[i] - '0'] < secretNum[guess[i] - '0']) {
+        //         cow++;
+        //         checked[guess[i] - '0']++;
+        //     }
+        // }
+        // return to_string(bull) + "A" + to_string(cow) + "B";
+    }
+};
