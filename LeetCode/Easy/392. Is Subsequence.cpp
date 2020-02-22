11086b934d4e73c53d37d2423f446a4a9e4b3b41
LaChimere <LaChimere@github.com>
2020-02-22 10:56:31 +0800

    Create 392. Is Subsequence.cpp

---
+++ LeetCode/Easy/392. Is Subsequence.cpp
@@ -0,0 +1,14 @@
+#include "tools"
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        size_t i = 0;
+        for (char c : s) {
+            i = t.find(c, i);
+            if (i == string::npos)
+                return false;
+            i++;
+        }
+        return true;
+    }
+};
