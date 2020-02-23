1a384f205d277998e42195561174baecc6d09c81
LaChimere <LaChimere@github.com>
2020-02-23 18:33:00 +0800

    Create 412. Fizz Buzz.cpp

---
+++ LeetCode/Easy/412. Fizz Buzz.cpp
@@ -0,0 +1,17 @@
+#include "tools"
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res(n);
+        for (int i = 0; i < n; i++) {
+            if ((i + 1) % 3 != 0 && (i + 1) % 5 != 0)
+                res[i] = to_string(i + 1);
+            else if ((i + 1) % 3 == 0 && (i + 1) % 5 != 0)
+                res[i] = "Fizz";
+            else if ((i + 1) % 3 != 0 && (i + 1) % 5 == 0)
+                res[i] = "Buzz";
+            else res[i] = "FizzBuzz";
+        }
+        return res;
+    }
+};
