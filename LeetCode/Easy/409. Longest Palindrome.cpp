0fe5bd97a9607c6c4e61d84d560f04a9f1fd1d80
LaChimere <LaChimere@github.com>
2020-02-23 17:47:32 +0800

    Update 409. Longest Palindrome.cpp

--- LeetCode/Easy/409. Longest Palindrome.cpp
+++ LeetCode/Easy/409. Longest Palindrome.cpp
@@ -10,9 +10,8 @@ public:
         int len = 0, single = 0;
         for (int i = 0; i < 26; i++) {
             if (lower[i] > 1) len += (lower[i] / 2) * 2;
-            if (lower[i] % 2) single = 1;
             if (upper[i] > 1) len += (upper[i] / 2) * 2;
-            if (upper[i] % 2) single = 1;
+            if ((lower[i] % 2) || (upper[i] % 2)) single = 1;
         }
         return len + single;
     }
