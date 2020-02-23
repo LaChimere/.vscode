9e9693f01772108e398478fb2e37c90867625445
LaChimere <LaChimere@github.com>
2020-02-23 16:29:34 +0800

    Create 404. Sum of Left Leaves.cpp

---
+++ LeetCode/Easy/404. Sum of Left Leaves.cpp
@@ -0,0 +1,18 @@
+#include "tools"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == nullptr) return 0;
+        if (root->left != nullptr && root->left->left == nullptr && root->left->right == nullptr)
+            return root->left->val + sumOfLeftLeaves(root->right);
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
