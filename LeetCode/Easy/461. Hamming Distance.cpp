791a1ae4fd2a71177a143f56448585c6d30fec41
LaChimere <LaChimere@github.com>
2020-02-26 13:26:27 +0800

    Update 461. Hamming Distance.cpp

--- LeetCode/Easy/461. Hamming Distance.cpp
+++ LeetCode/Easy/461. Hamming Distance.cpp
@@ -4,8 +4,8 @@ public:
     int hammingDistance(int x, int y) {
         int k = x ^ y, cnt = 0;
         while (k) {
-            if (k & 1) cnt++;
-            k >>= 1;
+            if (k & -k) cnt++;
+            k -= k & -k;
         }
         return cnt;
     }
