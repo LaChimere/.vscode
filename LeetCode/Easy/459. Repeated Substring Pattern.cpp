d23bd4b9df9f19c0a1c3f37617a0ce87005bd699
LaChimere <LaChimere@github.com>
2020-02-26 12:16:15 +0800

    Update 459. Repeated Substring Pattern.cpp

--- LeetCode/Easy/459. Repeated Substring Pattern.cpp
+++ LeetCode/Easy/459. Repeated Substring Pattern.cpp
@@ -10,18 +10,18 @@ public:
         // }
         // return false;
 
-        return (s + s).find(s, 1) != s.size();
+        // return (s + s).find(s, 1) != s.size();
         
-        // for (size_t len = 1; len <= s.length() / 2; len++) {
-        //     if (s.length() % len != 0) continue;
-        //     string pattern = s.substr(0, len);
-        //     size_t i = 1;
-        //     for (; i * len < s.length(); i++) {
-        //         string nowCheck = s.substr(i * len, len);
-        //         if (pattern != nowCheck) break;
-        //     }
-        //     if (i * len >= s.length()) return true;
-        // }
-        // return false;
+        for (size_t len = 1; len <= s.length() / 2; len++) {
+            if (s.length() % len != 0) continue;
+            string pattern = s.substr(0, len);
+            size_t i = 1;
+            for (; i * len < s.length(); i++) {
+                string nowCheck = s.substr(i * len, len);
+                if (pattern != nowCheck) break;
+            }
+            if (i * len >= s.length()) return true;
+        }
+        return false;
     }
 };
