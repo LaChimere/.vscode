0dcef9331591d2ed83295387254a8ad1193f3ee6
LaChimere <LaChimere@github.com>
2020-02-26 12:05:11 +0800

    Create 459. Repeated Substring Pattern.cpp

---
+++ LeetCode/Easy/459. Repeated Substring Pattern.cpp
@@ -0,0 +1,25 @@
+#include "tools"
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        // for (size_t len = 1; len <= s.length() / 2; len++) {
+        //     if (s.length() % len != 0) continue;
+        //     size_t i = len;
+        //     for (; i < s.length() && s[i % len] == s[i]; i++);
+        //     if (i == s.length()) return true;
+        // }
+        // return false;
+        
+        for (size_t len = 1; len <= s.length() / 2; len++) {
+            if (s.length() % len != 0) continue;
+            string pattern = s.substr(0, len);
+            size_t i = 1;
+            for (; i * len < s.length(); i++) {
+                string nowCheck = s.substr(i * len, len);
+                if (pattern != nowCheck) break;
+            }
+            if (i * len >= s.length()) return true;
+        }
+        return false;
+    }
+};
