aa5103fe6c837f792e44c3de4b3af4d4f4ef263b
LaChimere <LaChimere@github.com>
2020-02-26 16:15:17 +0800

    Create 463. Island Perimeter.cpp

---
+++ LeetCode/Easy/463. Island Perimeter.cpp
@@ -0,0 +1,17 @@
+#include "tools"
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int cnt = 0;
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[i]. size(); j++)
+                if (grid[i][j]) {
+                    cnt += 4;
+                    if (i - 1 >= 0 && grid[i - 1][j]) cnt -= 1;
+                    if (i + 1 < grid.size() && grid[i + 1][j]) cnt -= 1;
+                    if (j - 1 >= 0 && grid[i][j - 1]) cnt -= 1;
+                    if (j + 1 < grid[i].size() && grid[i][j + 1]) cnt -= 1;
+                }
+        return cnt;
+    }
+};
