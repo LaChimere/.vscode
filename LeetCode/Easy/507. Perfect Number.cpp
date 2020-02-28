f415c0e548703313c96cafbfb0261b4b82037420
LaChimere <LaChimere@github.com>
2020-02-28 13:50:17 +0800

    Update 507. Perfect Number.cpp

--- LeetCode/Easy/507. Perfect Number.cpp
+++ LeetCode/Easy/507. Perfect Number.cpp
@@ -6,7 +6,6 @@ public:
         for (int i = 2; i <= num / i; i++)
             if (num % i == 0)  a -= i + num / i;
         if (num != 1) a -= 1;
-        if (a == 0) return true;
-        else return false;
+        return a == 0;
     }
 };
