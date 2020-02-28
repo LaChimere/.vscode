f02281861bc8e5613b180ea9d3acb2a789076b9c
LaChimere <LaChimere@github.com>
2020-02-28 15:23:16 +0800

    Create 521. Longest Uncommon Subsequence I.cpp

---
+++ LeetCode/Easy/521. Longest Uncommon Subsequence I.cpp
@@ -0,0 +1,7 @@
+#include "tools"
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        return a == b ? -1 : (a.length() > b.length() ? a.length() : b.length());
+    }
+};
