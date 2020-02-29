4e4fb2411a13c058285db75dda31ec9b6774f9a3
LaChimere <LaChimere@github.com>
2020-02-29 14:21:10 +0800

    Update 541. Reverse String II.cpp

--- LeetCode/Easy/541. Reverse String II.cpp
+++ LeetCode/Easy/541. Reverse String II.cpp
@@ -4,7 +4,7 @@ public:
     string reverseStr(string s, int k) {
         if (s.length() <= k) {reverse(s.begin(), s.end()); return s;}
         reverse(s.begin(), s.begin() + k);
-        if (s.length() <= 2 * k) return s;
-        return s.substr(0, 2 * k) + reverseStr(s.substr(2 * k), k);
+        if (s.length() <= k << 1) return s;
+        return s.substr(0, k << 1) + reverseStr(s.substr(k << 1), k);
     }
 };
