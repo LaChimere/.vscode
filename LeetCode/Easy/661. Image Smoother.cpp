fe638d32a2ba44a5c42f7939f1f1c14287a2f63a
LaChimere <LaChimere@github.com>
2020-03-03 17:27:38 +0800

    Update 661. Image Smoother.cpp

--- LeetCode/Easy/661. Image Smoother.cpp
+++ LeetCode/Easy/661. Image Smoother.cpp
@@ -4,16 +4,16 @@ public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& M) {
         int row = M.size(), col = M[0].size();
         vector<vector<int>> res(row, vector<int>(col));
-        for (int i = 0; i < row * col; i++) {
-            int r = i / col, c = i % col;
+        for (int k = 0; k < row * col; k++) {
+            int r = k / col, c = k % col;
             int x1 = c, x2 = c, y1 = r, y2 = r, sum = 0;
             if (c - 1 >= 0) x1 = c - 1;
             if (c + 1 < col) x2 = c + 1;
             if (r - 1 >= 0) y1 = r - 1;
             if (r + 1 < row) y2 = r + 1;
-            for (int j = y1; j <= y2; j++) {
-                for (int k = x1; k <= x2; k++)
-                    sum += M[j][k];
+            for (int i = y1; i <= y2; i++) {
+                for (int j = x1; j <= x2; j++)
+                    sum += M[i][j];
             }
             int cnt = (y2 - y1 + 1) * (x2 - x1 + 1);
             res[r][c] = sum / cnt;
