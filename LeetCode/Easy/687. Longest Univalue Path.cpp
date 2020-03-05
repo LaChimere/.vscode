31e23892ac5011199566b2a79c70a8db56523a5c
LaChimere <LaChimere@github.com>
2020-03-05 15:40:16 +0800

    Create 687. Longest Univalue Path.cpp

---
+++ LeetCode/Easy/687. Longest Univalue Path.cpp
@@ -0,0 +1,30 @@
+#include "tools"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+private:
+    int arrowLength(TreeNode* root, int& path) {
+        if (!root) return 0;
+        int leftPath = arrowLength(root->left, path);
+        int rightPath = arrowLength(root->right, path);
+        int arrowLeft = 0, arrowRight = 0;
+        if (root->left && root->left->val == root->val)
+            arrowLeft += leftPath + 1;
+        if (root->right && root->right->val == root->val)
+            arrowRight += rightPath + 1;
+        path = max(path, arrowLeft + arrowRight);
+        return max(arrowLeft, arrowRight);
+    }
+public:
+    int longestUnivaluePath(TreeNode* root) {
+        int path = 0;
+        arrowLength(root, path);
+        return path;
+    }
+};
