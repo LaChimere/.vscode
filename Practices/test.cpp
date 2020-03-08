b053a427f5e6f7a82909e5d577be4fdb9b46bfd0
LaChimere <LaChimere@github.com>
2020-03-08 10:16:13 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -8,12 +8,37 @@
 #include <map>
 #include <set>
 #include <unordered_set>
+#include <queue>
 using namespace std;
 
+template<typename T> void print_queue(T& q) {
+    while (!q.empty()) {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+
+struct fruit {
+    string name;
+    int price;
+    fruit(string _name, int _price) : name(_name), price(_price) {}
+    friend bool operator<(const fruit& f1, const fruit& f2) {
+        return f1.price > f2.price;
+    }
+};
+
 int main() {
-    vector<int> v1({1, 2, 3}), v2({4, 5, 6});
-    swap(v1, v2);
-    for (int num : v1)
-        cout << num << " ";
+    priority_queue<fruit> q;
+    fruit f1("Apple", 3);
+    fruit f2("Pear", 4);
+    fruit f3("Peach", 1);
+    q.push(f1);
+    q.push(f2);
+    q.push(f3);
+    while (!q.empty()) {
+        cout << q.top().name << " : " << q.top().price << endl;
+        q.pop();
+    }
     return 0;
 }
