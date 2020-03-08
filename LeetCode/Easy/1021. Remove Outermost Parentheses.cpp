0143edeb460898002292498e1023b98b81321110
LaChimere <LaChimere@github.com>
2020-03-08 14:56:40 +0800

    Create 1021. Remove Outermost Parentheses.cpp

---
+++ LeetCode/Easy/1021. Remove Outermost Parentheses.cpp
@@ -0,0 +1,25 @@
+#include "tools"
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        if (S.empty()) return "";
+        stack<char> st;
+        string res;
+        int i = 0;
+        st.push(S[i++]);
+        while (i < S.length()) {
+            if (!st.empty() && S[i] == '(') {
+                res.push_back(S[i]);
+                st.push(S[i]);
+            } else if (st.empty() && S[i] == '(') {
+                st.push(S[i]);
+            } else {
+                st.pop();
+                if (!st.empty())
+                    res.push_back(S[i]);
+            }
+            i++;
+        }
+        return res;
+    }
+};
