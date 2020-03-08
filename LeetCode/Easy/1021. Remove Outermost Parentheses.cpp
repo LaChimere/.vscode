341c19ac258ecf6e454118074101ea4c01f6e06e
LaChimere <LaChimere@github.com>
2020-03-08 15:06:23 +0800

    Update 1021. Remove Outermost Parentheses.cpp

--- LeetCode/Easy/1021. Remove Outermost Parentheses.cpp
+++ LeetCode/Easy/1021. Remove Outermost Parentheses.cpp
@@ -2,21 +2,31 @@
 class Solution {
 public:
     string removeOuterParentheses(string S) {
-        if (S.empty()) return "";
-        stack<char> st;
+        int left = 1, right = 0;
         string res;
-        int i = 0;
-        st.push(S[i++]);
-        while (i < S.length()) {
-            if (S[i] == '(') {
-                if (!st.empty()) res.push_back(S[i]);
-                st.push(S[i]);
-            } else {
-                st.pop();
-                if (!st.empty()) res.push_back(S[i]);
-            }
-            i++;
+        for (int i = 1; i < S.length(); i++) {
+            if (S[i] == '(') left++;
+            else right++;
+            if (right != left) res.push_back(S[i]);
+            else i++, left = 1, right = 0;
         }
         return res;
+
+        // if (S.empty()) return "";
+        // stack<char> st;
+        // string res;
+        // int i = 0;
+        // st.push(S[i++]);
+        // while (i < S.length()) {
+        //     if (S[i] == '(') {
+        //         if (!st.empty()) res.push_back(S[i]);
+        //         st.push(S[i]);
+        //     } else {
+        //         st.pop();
+        //         if (!st.empty()) res.push_back(S[i]);
+        //     }
+        //     i++;
+        // }
+        // return res;
     }
 };
