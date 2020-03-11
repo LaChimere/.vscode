ada67bfe384b4e7c9d7c47c95a60416817b655ca
LaChimere <LaChimere@github.com>
2020-03-11 16:34:37 +0800

    Update 784. Letter Case Permutation.cpp

--- LeetCode/Easy/784. Letter Case Permutation.cpp
+++ LeetCode/Easy/784. Letter Case Permutation.cpp
@@ -1,34 +1,34 @@
 #include "tools"
 class Solution {
-private:
-    void permuteString(vector<string>& str, string s, int start, int len) {
-        if (start >= len) {str.push_back(s); return;}
-        int i;
-        for (i = start; i < len && !isalpha(s[i]); i++);
-        // 不改
-        permuteString(str, s, i + 1, len);
-        // 改
-        if (i < len) {
-            s[i] ^= 1 << 5;
-            permuteString(str, s, i + 1, len);
-        }
-    }
+// private:
+//     void permuteString(vector<string>& str, string s, int start, int len) {
+//         if (start >= len) {str.push_back(s); return;}
+//         int i;
+//         for (i = start; i < len && !isalpha(s[i]); i++);
+//         // 不改
+//         permuteString(str, s, i + 1, len);
+//         // 改
+//         if (i < len) {
+//             s[i] ^= 1 << 5;
+//             permuteString(str, s, i + 1, len);
+//         }
+//     }
 public:
     vector<string> letterCasePermutation(string S) {
-        vector<string> str;
-        permuteString(str, S, 0, S.length());
-        return str;
+        // vector<string> str;
+        // permuteString(str, S, 0, S.length());
+        // return str;
         
-        // vector<string> res({S});
-        // for (int i = 0; i < S.length(); i++)
-        //     if (isalpha(S[i])) {
-        //         int size = res.size();
-        //         for (int j = 0; j < size; j++) {
-        //             string temp = res[j];
-        //             temp[i] ^= 1 << 5;
-        //             res.emplace_back(move(temp));
-        //         }
-        //     }
-        // return res;
+        vector<string> res({S});
+        for (int i = 0; i < S.length(); i++)
+            if (isalpha(S[i])) {
+                int size = res.size();
+                for (int j = 0; j < size; j++) {
+                    string temp = res[j];
+                    temp[i] ^= 1 << 5;
+                    res.emplace_back(move(temp));
+                }
+            }
+        return res;
     }
 };
