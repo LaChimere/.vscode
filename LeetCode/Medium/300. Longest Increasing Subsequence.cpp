4ccdf0c7a1fa05afa899db0dc083e133081d4106
LaChimere <LaChimere@github.com>
2020-03-14 11:06:39 +0800

    Create 300. Longest Increasing Subsequence.cpp

---
+++ LeetCode/Medium/300. Longest Increasing Subsequence.cpp
@@ -0,0 +1,17 @@
+#include "tools"
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int len = nums.size();
+        vector<int> dp(len, 1);
+        int res = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j])
+                    dp[i] = max(dp[i], dp[j] + 1);
+            }
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
