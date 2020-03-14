35df2288895ed84294b72e614d7b403cc1265f89
LaChimere <LaChimere@github.com>
2020-03-14 12:41:01 +0800

    Create 892. Surface Area of 3D Shapes.cpp

---
+++ LeetCode/Easy/892. Surface Area of 3D Shapes.cpp
@@ -0,0 +1,19 @@
+#include "tools"
+class Solution {
+public:
+    int surfaceArea(vector<vector<int>>& grid) {
+        int sum = 0, N = grid.size();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j]) {
+                    sum += 6 + 4 * (grid[i][j] - 1);
+                    if (i - 1 >= 0) sum -= min(grid[i - 1][j], grid[i][j]);
+                    if (i + 1 < N) sum -= min(grid[i + 1][j], grid[i][j]);
+                    if (j - 1 >= 0) sum -= min(grid[i][j - 1], grid[i][j]);
+                    if (j + 1 < N) sum -= min(grid[i][j + 1], grid[i][j]);
+                }
+            }
+        }
+        return sum;
+    }
+};
