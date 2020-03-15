da718cef5da7a8dc50cff84808b95c545113a78c
LaChimere <LaChimere@github.com>
2020-03-15 10:59:28 +0800

    Create 746. Min Cost Climbing Stairs.cpp

---
+++ LeetCode/Easy/746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,12 @@
+#include "tools"
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = cost[0], dp[1] = cost[1];
+        for (int i = 2; i < n; i++)
+            dp[i] = min(dp[i - 2] + cost[i], dp[i - 1] + cost[i]);
+        return min(dp[n - 2], dp[n - 1]);
+    }
+};
