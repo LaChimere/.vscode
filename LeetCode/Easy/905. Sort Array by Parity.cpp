cc626af8e877b62b68957890b25ee2547b748097
LaChimere <LaChimere@github.com>
2020-03-16 16:18:32 +0800

    Create 905. Sort Array by Parity.cpp

---
+++ LeetCode/Easy/905. Sort Array by Parity.cpp
@@ -0,0 +1,13 @@
+#include "tools"
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        int i = 0, j = A.size() - 1;
+        while (i < j) {
+            if (A[i] % 2 == 1 && A[j] % 2 == 0) swap(A[i], A[j]);
+            if (A[i] % 2 == 0) i++;
+            if (A[j] % 2 == 1) j--;
+        }
+        return A;
+    }
+};
