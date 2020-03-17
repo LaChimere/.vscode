8b3381066eedb04e3336fcec9f94fe96aa5f35eb
LaChimere <LaChimere@github.com>
2020-03-17 13:18:51 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -11,31 +11,29 @@
 #include <queue>
 using namespace std;
 
-struct info {
-    string name, address;
-    int age;
-    info(string _name, string _address, int _age) : name(_name), address(_address), age(_age) {
-        cout << "constructed" << endl;
+vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& queries) {
+    vector<int> ans(queries.size());
+    int sum = 0;
+    for (int n : A)
+        if (n % 2 == 0) sum += n;
+    for (int i = 0; i < queries.size(); i++) {
+        int val = queries[i][0], index = queries[i][1];
+        int curAi = A[index] + val;
+        if (curAi % 2 == 0) {
+            sum += val;
+            if (A[index] % 2 != 0) sum += A[index];
+        } else if (A[index] % 2 == 0) sum -= A[index];
+        ans[i] = sum;
+        A[index] = curAi;
     }
-    info(info&& p) : name(move(p.name)), address(move(p.address)), age(p.age) {
-        cout << "copied" << endl;
-    }
-};
-
-int lengthOfLIS(vector<int>& nums) {
-    int len = nums.size();
-    vector<int> dp(len, 1);
-    int res = 0;
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < i; j++)
-            if (nums[i] > nums[j]) dp[i] = max(1, dp[j] + 1);
-        res = max(res, dp[i]);
-    }
-    return res;
+    return ans;
 }
 
 int main() {
-    vector<int> nums({1, 3, 6, 7, 9, 4, 10, 5, 6});
-    cout << lengthOfLIS(nums);
+    vector<int> A({-10, 2, -4, 5, -3, 3});
+    vector<vector<int>> queries({{-4, 2}, {9, 0}, {-8, 1}, {5, 4}, {1, 4}, {9, 0}});
+    vector<int> res(sumEvenAfterQueries(A, queries));
+    for (int n : res)
+        cout << n << " ";
     return 0;
 }
