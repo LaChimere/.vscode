32aec6edafa8ed5d282afd0e90da7f2ca6ce544f
LaChimere <LaChimere@github.com>
2020-03-18 15:31:23 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -11,17 +11,17 @@
 #include <queue>
 using namespace std;
 
-int numPairsDivisibleBy60(vector<int>& time) {
-    vector<vector<int>> count(501);
-    int len = time.size(), cnt = 0;
-    for (int i = 0; i < len; i++) count[time[i]].push_back(i);
-    for (int i = 0; i < len; i++) {
-        for (int k = 1; k <= 9; k++) {
-            int check = k * 60 - time[i];
-            if (check > 500) break;
-            for (int j : count[check])
-                if (i < j) cnt++;
+int main() {
+    vector<int> v{1, 3, 4, 5, 6, 0, 4, 8, 0, 2};
+    auto it = v.begin();
+    for (; it != v.end(); it++) {
+        if (*it == 0) {
+            auto temp = it;
+            v.insert(it, 0);
+            it = temp;
         }
     }
-    return cnt;
+    for (int n : v)
+        cout << n << " ";
+    return 0;
 }
