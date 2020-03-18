d21cba11d635d46fb10d5a023eef59300a55cdff
LaChimere <LaChimere@github.com>
2020-03-18 18:13:38 +0800

    Update 1170. Compare Strings by Frequency of the Smallest Character.cpp

--- LeetCode/Easy/1170. Compare Strings by Frequency of the Smallest Character.cpp
+++ LeetCode/Easy/1170. Compare Strings by Frequency of the Smallest Character.cpp
@@ -15,13 +15,19 @@ private:
     }
 public:
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
-        vector<int> W(words.size()), res(queries.size(), 0);
-        for (int i = 0; i < words.size(); i++) W[i] = f(words[i]);
-        for (int i = 0; i < queries.size(); i++) {
-            int num = f(queries[i]);
-            for (int w : W)
-                if (w > num) res[i]++;
-        }
+        vector<int> res(queries.size()), count(12, 0);
+        for (string w : words) count[f(w)]++;
+        for (int i = 9; i > 0; i--) count[i] += count[i + 1];
+        for (int i = 0; i < queries.size(); i++) res[i] = count[f(queries[i]) + 1];
         return res;
+        
+        // vector<int> W(words.size()), res(queries.size(), 0);
+        // for (int i = 0; i < words.size(); i++) W[i] = f(words[i]);
+        // for (int i = 0; i < queries.size(); i++) {
+        //     int num = f(queries[i]);
+        //     for (int w : W)
+        //         if (w > num) res[i]++;
+        // }
+        // return res;
     }
 };
