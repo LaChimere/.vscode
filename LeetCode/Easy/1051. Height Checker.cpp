c086c3f23dfcb409f7593296482914d49e3cf934
LaChimere <LaChimere@github.com>
2020-03-18 14:27:47 +0800

    Create 1051. Height Checker.cpp

---
+++ LeetCode/Easy/1051. Height Checker.cpp
@@ -0,0 +1,12 @@
+#include "tools"
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> h = heights;
+        sort(h.begin(), h.end());
+        int cnt = 0;
+        for (int i = 0; i < heights.size(); i++)
+            if (heights[i] != h[i]) cnt++;
+        return cnt;
+    }
+};
