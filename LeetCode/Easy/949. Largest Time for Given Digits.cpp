af64efb97398a3439c8f46c918cf602df3eff273
LaChimere <LaChimere@github.com>
2020-03-23 11:52:56 +0800

    Create 949. Largest Time for Given Digits.cpp

---
+++ LeetCode/Easy/949. Largest Time for Given Digits.cpp
@@ -0,0 +1,28 @@
+#include "tools"
+class Solution {
+public:
+    string largestTimeFromDigits(vector<int>& A) {
+        int ans = -1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (j != i) {
+                    for (int k = 0; k < 4; k++) {
+                        if (k != i && k != j) {
+                            int l = 6 - i - j - k;
+                            int hours = 10 * A[i] + A[j];
+                            int mins = 10 * A[k] + A[l];
+                            if (hours < 24 && mins < 60)
+                                ans = max(ans, hours * 60 + mins);
+                        }
+                    }
+                }
+            }
+        }
+        if (ans >= 0) {
+            char res[6];
+            sprintf(res, "%02d:%02d", ans / 60, ans % 60);
+            return res;
+        }
+        return "";
+    }
+};
