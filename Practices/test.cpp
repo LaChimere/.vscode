a0cd15d8e1b897b57a9e126c08891a8fbda8e0b9
LaChimere <LaChimere@github.com>
2020-03-23 14:45:59 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -12,12 +12,5 @@
 using namespace std;
 
 int main() {
-    bitset<32> num = bitset<32>(5);
-    int k = 0;
-    for (int i = 0; i < 32; i++) {
-        cout << num[i];
-        k = (k + 1) % 4;
-        if (k == 0) cout << " ";
-    }
     return 0;
 }
