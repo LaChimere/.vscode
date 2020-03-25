b75b19ec69834866690510960f3e3c8c59d480f8
LaChimere <LaChimere@github.com>
2020-03-25 11:34:46 +0800

    Create 859. Buddy Strings.cpp

---
+++ LeetCode/Easy/859. Buddy Strings.cpp
@@ -0,0 +1,24 @@
+#include "tools"
+class Solution {
+public:
+    bool buddyStrings(string A, string B) {
+        if (A.length() != B.length()) return false;
+        if (A == B) {
+            vector<int> count(26, 0);
+            for (char& c : A) count[c - 'a']++;
+            for (int cnt : count) 
+                if (cnt > 1) return true;
+            return false;
+        } else {
+            int first = -1, second = -1;
+            for (int i = 0; i < A.length(); i++) {
+                if (A[i] != B[i]) {
+                    if (first == -1) first = i;
+                    else if (second == -1) second = i;
+                    else return false;
+                }
+            }
+            return second != -1 && A[first] == B[second] && A[second] == B[first];
+        }
+    }
+};
