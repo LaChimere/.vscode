13c49c64f7af7f7c3a36e098dcae6dacfe799e22
LaChimere <LaChimere@github.com>
2020-03-26 15:10:15 +0800

    Create 1385. Find the Distance Value Between Two Arrays.cpp

---
+++ LeetCode/Easy/1385. Find the Distance Value Between Two Arrays.cpp
@@ -0,0 +1,20 @@
+#include "tools"
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        int len1 = arr1.size(), len2 = arr2.size();
+        sort(arr2.begin(), arr2.end());
+        int cnt = 0;
+        for (int num : arr1) {
+            int left = 0, right = len2 - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (num < arr2[mid]) right = mid;
+                else left = mid + 1;
+            }
+            int a = arr2[right], b = arr2[max(0, right - 1)], c = arr2[min(len2 - 1, right + 1)];
+            if (abs(num - a) > d && abs(num - b) > d && abs(num - c) > d) cnt++;
+        }
+        return cnt;
+    }
+};
