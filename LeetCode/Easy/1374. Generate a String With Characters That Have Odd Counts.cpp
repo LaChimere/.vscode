4dc2aa2bcde427bbca51b852f2a6d691300fa92b
LaChimere <LaChimere@github.com>
2020-03-26 11:55:48 +0800

    Create 1374. Generate a String With Characters That Have Odd Counts.cpp

---
+++ LeetCode/Easy/1374. Generate a String With Characters That Have Odd Counts.cpp
@@ -0,0 +1,8 @@
+#include "tools"
+class Solution {
+public:
+    string generateTheString(int n) {
+        if (n & 1) return string(n, 'a');
+        else return string(n - 1, 'a') + "b";
+    }
+};
