b91915dc4c61f2e0440419d9336f8c50ecaae320
LaChimere <chingkun_lin@126.com>
2020-03-30 15:03:56 +0800

    Update POJ1958.cpp

--- Contest Hunter/POJ1958.cpp
+++ Contest Hunter/POJ1958.cpp
@@ -1,22 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int d[13], f[13];
-
-int main() {
+int main()
+{
+    int d[13], f[13];
     d[1] = 1;
-    for (int i = 2; i <= 12; i++) {
+    for (int i = 2; i <= 12; i++)
         d[i] = 2 * d[i - 1] + 1;
-    }
-    f[1] = 1;
-    cout << f[1] << endl;
-    for (int num = 2; num <= 12; num++) {
-        int minAns = 0x3f3f3f3f;
-        for (int i = 1; i < num; i++) {
-            minAns = min(minAns, 2 * f[i] + d[num - i]);
-        }
-        f[num] = minAns;
-        cout << f[num] << endl;
-    }
+    memset(f, 0x3f, sizeof(f));
+    f[0] = 0;
+    for (int i = 1; i <= 12; i++)
+        for (int j = 0; j < i; j++)
+            f[i] = min(f[i], 2 * f[j] + d[i - j]);
+    for (int i = 1; i <= 12; i++)
+        cout << f[i] << endl;
     return 0;
 }
