2a4eb51fa7671242c085170f0ec96b3d63cba381
LaChimere <chingkun_lin@126.com>
2020-03-30 11:10:21 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -1,19 +1,5 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cctype>
-#include <bitset>
-#include <string>
-#include <unordered_map>
-#include <map>
-#include <set>
-#include <unordered_set>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    string s = "jinlingxia";
-    s += 'a';
-    cout << s;
-    return 0;
 }
