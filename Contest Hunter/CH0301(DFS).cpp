3790c618afe853faa12351c87128778b45a42db0
LaChimere <chingkun_lin@126.com>
2020-03-30 11:53:17 +0800

    Create CH0301(DFS).cpp

---
+++ Contest Hunter/CH0301(DFS).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+void dfs(int u, int state) 
+{
+    if (u == n) 
+    {
+        for (int i = 0; i < n; i++)
+            if (state >> i & 1)
+                cout << i + 1 << " ";
+        cout << endl;
+        return;
+    }
+    dfs(u + 1, state);
+    dfs(u + 1, state | 1 << u);
+}
+
+int main()
+{
+    cin >> n;
+    dfs(0, 0);
+    return 0;
+}
