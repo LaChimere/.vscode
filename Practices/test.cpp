be3dad44089713c8e6e9440f94ee129b89decd0b
LaChimere <chingkun_lin@126.com>
2020-04-03 11:16:50 +0800

    Update test.cpp

--- Practices/test.cpp
+++ Practices/test.cpp
@@ -1,5 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int main() {
+template <class T>
+void printElems(const T& t)
+{
+    if (t.empty())
+    {
+        cout << "Empty" << endl;
+        return;
+    }
+    for (int i = 0; i < t.size(); i++)
+    {
+        cout << t[i];
+        if (i < t.size() - 1)
+            cout << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> nums{1, 5, 3, 3, 5, 1, 1, 5};
+    sort(nums.begin(), nums.end());	// 1 1 1 3 3 5 5 5
+    for (const int& num : nums)
+        cout << num << " ";
+    cout << endl;
+    auto low = lower_bound(nums.begin(), nums.end(), 3);
+    auto up = upper_bound(nums.begin(), nums.end(), 3);
+
+    nums.insert(low, 2);
+    nums.insert(up, 4);
+    for (int i = 0; i < nums.size(); i++)
+        cout << nums[i] << ' ';
+
+    return 0;
 }
