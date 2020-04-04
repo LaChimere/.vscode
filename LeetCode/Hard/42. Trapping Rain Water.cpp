d9089e55bf5309f060fbda59d1874ac1e471d3ce
LaChimere <chingkun_lin@126.com>
2020-04-04 11:04:05 +0800

    Create 42. Trapping Rain Water.cpp

---
+++ LeetCode/Hard/42. Trapping Rain Water.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution_DP {
+public:
+    int trap(vector<int>& height) {
+        int sum = 0, len = height.size();
+        vector<int> leftMax(len, 0), rightMax(len, 0);
+        for (int i = 1; i < len; i++)
+            leftMax[i] = max(leftMax[i - 1], height[i - 1]);
+        for (int i = len - 2; i >= 0; i--)
+            rightMax[i] = max(rightMax[i + 1], height[i + 1]);
+        for (int i = 1; i < len - 1; i++) {
+            int minLevel = min(leftMax[i], rightMax[i]);
+            if (minLevel > height[i])
+                sum += minLevel - height[i];
+        }
+        return sum;
+    }
+};
+
+class Solution_TwoPointers {
+public:
+    int trap(vector<int>& height) {
+        int sum = 0, len = height.size();
+        int leftMax = 0, rightMax = 0;
+        int left = 1, right = len - 2;
+        for (int i = 1; i < len - 1; i++) {
+            if (height[left - 1] < height[right + 1]) {
+                leftMax = max(leftMax, height[left - 1]);
+                int min = leftMax;
+                if (min > height[left])
+                    sum += min - height[left];
+                left++;
+            } else {
+                rightMax = max(rightMax, height[right + 1]);
+                int min = rightMax;
+                if (min > height[right])
+                    sum += min - height[right];
+                right--;
+            }
+        }
+        return sum;
+    }
+};
