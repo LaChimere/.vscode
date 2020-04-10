8c8ce6d2c983e4b0bf0f1078186995786e10389c
LaChimere <chingkun_lin@126.com>
2020-04-10 10:59:04 +0800

    Update 151. Reverse Words in a String.cpp

--- LeetCode/Medium/151. Reverse Words in a String.cpp
+++ LeetCode/Medium/151. Reverse Words in a String.cpp
@@ -16,3 +16,24 @@ public:
         return res;
     }
 };
+
+class Solution2 {
+public:
+    string reverseWords(string s) {
+        while (!s.empty() && s.front() == ' ') s.erase(s.begin());
+        while (!s.empty() && s.back() == ' ') s.erase(s.begin() + s.length() - 1);
+        int anchor = s.length(), cur;
+        string res;
+        for (cur = s.length() - 1; cur >= 0; cur--) {
+            if (cur == 0) {
+                res += string(s.begin(), s.begin() + anchor);
+                continue;
+            }
+            if (s[cur] != ' ') continue;
+            res += string(s.begin() + cur + 1, s.begin() + anchor) + " ";
+            while (cur >= 0 && s[cur] == ' ') cur--;
+            anchor = ++cur;
+        }
+        return res;
+    }
+};
