e5fd94b45ee5a657e75618aba9aa0fda4e5ab4d6
LaChimere <chingkun_lin@126.com>
2020-04-18 09:58:20 +0800

    Create 11. Container With Most Water.cpp

---
+++ LeetCode/Medium/11. Container With Most Water.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0, right = height.size() - 1, res = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                res = max(res, height[left] * (right - left));
+                left++;
+            } else {
+                res = max(res, height[right] * (right - left));
+                right--;
+            }
+        }
+        return res;
+    }
+};
