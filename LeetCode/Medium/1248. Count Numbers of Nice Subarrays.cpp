8ccc3f6a43168f598e2e57d77859c71f0912998e
LaChimere <chingkun_lin@126.com>
2020-04-21 10:22:06 +0800

    Update 1248. Count Numbers of Nice Subarrays.cpp

--- LeetCode/Medium/1248. Count Numbers of Nice Subarrays.cpp
+++ LeetCode/Medium/1248. Count Numbers of Nice Subarrays.cpp
@@ -4,11 +4,13 @@ using namespace std;
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
+        // 利用前缀和的思想, 求出 nums 中奇数个数的前缀和
         vector<int> odd;
         odd.push_back(0);
         for (int num : nums)
             odd.push_back(odd.back() + (num & 1));
         
+        //利用 Two Sum 的思想, 找出 odd 中满足 odd[j] - odd[i] == k 的对数
         unordered_map<int, int> hash;
         int cnt = 0;
         for (int i : odd) {
