048246513ce37985b4ae6642ffd806790a6132e3
LaChimere <chingkun_lin@126.com>
2020-04-25 09:22:18 +0800

    Update 46. Permutations.cpp

--- LeetCode/Medium/46. Permutations.cpp
+++ LeetCode/Medium/46. Permutations.cpp
@@ -5,26 +5,26 @@ class Solution {
 private:
     unordered_set<int> chosen;
     vector<int> ans;
-    void getPermutation(const vector<int>& nums, vector<vector<int>>& res, int i) {
+    void getPermutation(const vector<int>& nums, vector<vector<int>>& res) {
         if (ans.size() == nums.size()) {
             res.emplace_back(ans);
             return;
         }
         
-        for (int k = 0; k < nums.size(); k++) {
-            if (chosen.find(nums[k]) != chosen.end())
+        for (int i = 0; i < nums.size(); i++) {
+            if (chosen.find(nums[i]) != chosen.end())
                 continue;
-            ans.push_back(nums[k]);
-            chosen.insert(nums[k]);
-            getPermutation(nums, res, k + 1);
-            chosen.erase(nums[k]);
+            ans.push_back(nums[i]);
+            chosen.insert(nums[i]);
+            getPermutation(nums, res);
+            chosen.erase(nums[i]);
             ans.pop_back();
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        getPermutation(nums, res, 0);
+        getPermutation(nums, res);
         return res;
     }
 };
